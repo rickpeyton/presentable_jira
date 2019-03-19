5984dec7d1334358b29a6a1a67da1ccf76c00004
Rick Peyton <rick.peyton@daveramsey.com>
2019-03-19 11:02:02 -0500

    Release 0.3.0 Minor UI Improvements

--- presentable.js
+++ presentable.js
@@ -1,5 +1,7 @@
-document.getElementById("ghx-pool").style.paddingTop="0";
+document.getElementById("content").style.marginLeft="1rem";
+document.getElementById("content").style.marginRight="1rem";
 document.getElementById("ghx-header").remove();
 document.getElementById("ghx-quick-filters").remove();
+document.getElementById("navigation-app").remove();
 var currentHeight = window.innerHeight + "px";
 document.getElementById("ghx-work").style.height=currentHeight;
