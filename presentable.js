ffe1017617c45ed958afb51b1eef6b9b5fd7fee9
Rick Peyton <rick.peyton@daveramsey.com>
2019-03-19 10:49:18 -0500

    Release v 0.2.0 Move JS to sepearate file and calculate inner height

---
+++ presentable.js
@@ -0,0 +1,5 @@
+document.getElementById("ghx-pool").style.paddingTop="0";
+document.getElementById("ghx-header").remove();
+document.getElementById("ghx-quick-filters").remove();
+var currentHeight = window.innerHeight + "px";
+document.getElementById("ghx-work").style.height=currentHeight;
